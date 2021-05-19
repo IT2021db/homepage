30008bb799d5c29b749485479b527387ff3d92fb
Dorota Borowska <youcode2021db@gmail.com>
2021-05-19 11:28:34 +0200

    on function changed

--- js/modul4a.js
+++ js/modul4a.js
@@ -1,15 +1,20 @@
-console.log("Hello!");
 {
-let nextColorName = document.querySelector(".js-nextColorName");
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let body = document.querySelector(".body");
-
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-    if (body.classList.contains("body--dark")) {
-        nextColorName.innerText = "jasne";
-    } else {
-        nextColorName.innerText = "ciemne";
+    const welcome = () => {
+        console.log("Hello!");
     }
-})
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton")
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+    }
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const nextColorName = document.querySelector(".js-nextColorName");
+        body.classList.toggle("body--dark");
+        nextColorName.innerText = body.classList.contains("body--dark") ? "jasne" : "ciemne";
+    }
+    welcome();
+    init();
+
+
+
 }
